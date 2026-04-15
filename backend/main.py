e2f435f808de50956e100e33ec9e87417fc21458
balapavi961-cloud <balapavi961@gmail.com>
2026-04-15 09:22:58 +0530

    added chat api and documents api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from routers import auth, tenant,superadmin
+from routers import auth, tenant,superadmin,chat,documents
 
 load_dotenv()
 
@@ -21,6 +21,8 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(tenant.router)
 app.include_router(superadmin.router)
+app.include_router(chat.router)
+app.include_router(documents.router)
 
 @app.get("/health")
 def health():
