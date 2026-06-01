6e113855f1ad31151446a6abb70ed570b7c491dd
prakash2819 <gopinatherg@gmail.com>
2026-06-01 16:26:38 +0530

    Updated UI

--- frontend/src/components/EmployeeLayout.jsx
+++ frontend/src/components/EmployeeLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo1.png'
 import {
   MessageSquare, Plus, Trash2,
   LogOut, Menu, X, BrainCircuit
